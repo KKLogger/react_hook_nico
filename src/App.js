4b33197e33d047e9a0c1af1d9ec927eda5332826
KKLogger <koc0819@gmail.com>
2020-12-27 23:39:01 +0900

    useState and useInput

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+  return { value, onChange };
+};
+const App = () => {
+  const [item, setItem] = useState(1);
+  const name = useInput("Mr.");
+
+  // useState 는 array 형태로 값을 반환 [0] : 값 [1] : 값 수정 함수
+  // <input placeholder={"Name"} value={name.value} onChange={name.onChange} /> 는
+  // <input placeholder={"Name"} {...name} /> 으로 대체될 수 있습니다.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"}>
+      <h1>Hello React Hook!</h1>
+      <input placeholder={"Name"} {...name} />
     </div>
   );
-}
+};
 
 export default App;
